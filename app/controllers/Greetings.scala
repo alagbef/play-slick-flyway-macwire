628a0740d0bf6429959ba96c27c0ebb19385ee10
Fayimora Femi-Balogun <fayi@fayimora.com>
2017-11-18 12:42:41 +0000

    Initial project generation

---
+++ app/controllers/Greetings.scala
@@ -0,0 +1,32 @@
+package controllers
+
+import akka.actor.ActorSystem
+import play.api.mvc.{AbstractController, ControllerComponents}
+
+import scala.concurrent.Future
+
+/**
+  * Created by Lloyd on 12/6/16.
+  *
+  * Copyright 2016
+  */
+class Greetings(controllerComponents: ControllerComponents)(implicit actorSystem: ActorSystem)
+    extends AbstractController(controllerComponents) {
+
+  import actorSystem.dispatcher
+
+  // Essentially copied verbatim from the SIRD example
+  def hello(to: String) = Action {
+    Ok(s"Hello $to")
+  }
+
+  /*
+     Use Action.async to return a Future result (sqrt can be intense :P)
+     Note the use of double(num) to bind only numbers (built-in :)
+   */
+  def sqrt(num: Double) = Action.async {
+    Future {
+      Ok(Math.sqrt(num).toString)
+    }
+  }
+}
---
+++ app/wiring/AppComponents.scala
@@ -0,0 +1,25 @@
+package wiring
+
+import play.api.ApplicationLoader.Context
+import play.api._
+import com.softwaremill.macwire._
+import _root_.controllers.Greetings
+
+import router.Routes
+
+class AppComponents(context: Context)
+    extends BuiltInComponentsFromContext(context)
+    with NoHttpFiltersComponents {
+
+  private implicit def as = actorSystem
+
+  // Controllers
+  private lazy val greetings = wire[Greetings]
+
+  // Router
+  lazy val router = {
+    val routePrefix: String = "/"
+    wire[Routes]
+  }
+
+}
